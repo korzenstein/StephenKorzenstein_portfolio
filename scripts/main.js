810d91c7b54ac1bf5de89d78278837a9d2c86c1b
Stephen Korzenstein <skorzenstein@gmail.com>
2022-04-17 13:55:27 -0400

    added media q's for mobile

--- scripts/main.js
+++ scripts/main.js
@@ -1,71 +1,66 @@
 // Navigation Toggle Button Functionality (from Project 1)
 const nav = document.querySelector(".navigation");
 function navBar() {
-    nav.classList.toggle("navActive");
+  nav.classList.toggle("navActive");
 }
-
 
 // Sections
-const homeSection = document.querySelector('.home');
-const aboutSection = document.querySelector('.about')
-const contactSection = document.querySelector('.contact')
-const portfolioSection = document.querySelector('.portfolio')
+const homeSection = document.querySelector(".home");
+const aboutSection = document.querySelector(".about");
+const contactSection = document.querySelector(".contact");
+const portfolioSection = document.querySelector(".portfolio");
 // Links
-const aboutLink = document.querySelector('.aboutLink')
-const homeLink = document.querySelector('.homeLink')
-const portfolioLink = document.querySelector('.portfolioLink')
-const contactLink = document.querySelector('.contactLink')
+const aboutLink = document.querySelector(".aboutLink");
+const homeLink = document.querySelector(".homeLink");
+const portfolioLink = document.querySelector(".portfolioLink");
+const contactLink = document.querySelector(".contactLink");
 
 const pageChange = (section) => {
-    homeSection.classList.remove('active')
-    aboutSection.classList.remove('active')
-    contactSection.classList.remove('active')
-    portfolioSection.classList.remove('active')
-    section.classList.add('active')
-    nav.classList.toggle("navActive");
+  homeSection.classList.remove("active");
+  aboutSection.classList.remove("active");
+  contactSection.classList.remove("active");
+  portfolioSection.classList.remove("active");
+  section.classList.add("active");
+  nav.classList.toggle("navActive");
+};
+aboutLink.addEventListener("click", () => {
+  pageChange(aboutSection);
+});
 
-}
-aboutLink.addEventListener('click', () => {
-    pageChange(aboutSection);
-})
+homeLink.addEventListener("click", () => {
+  pageChange(homeSection);
+});
 
-homeLink.addEventListener('click', () => {
-    pageChange(homeSection);
-})
+contactLink.addEventListener("click", () => {
+  pageChange(contactSection);
+});
 
-contactLink.addEventListener('click', () => {
-    pageChange(contactSection);
-})
-
-portfolioLink.addEventListener('click', () => {
-    pageChange(portfolioSection);
-})
+portfolioLink.addEventListener("click", () => {
+  pageChange(portfolioSection);
+});
 
 // Portfolio container
 
-const projButton = document.querySelectorAll('.projButton')
-const projects = document.querySelectorAll('.project')
-
+const projButton = document.querySelectorAll(".projButton");
+const projects = document.querySelectorAll(".project");
 
 projButton.forEach((bttn) => {
   bttn.addEventListener("click", () => {
-    
     // console.log(bttn.id)
     let chosen = bttn.id;
-    console.log(chosen)
+    console.log(chosen);
     // let whatToDisplay = [...projects].filter(p => p.classList.contains(chosen))
     // console.log(whatToDisplay)
+    projects.forEach((project) => {
+      console.log(project);
+      project.classList.remove("display");
+      if (project.classList.contains(chosen)) {
+        // project.classList.add('display')
+        console.log(project.classList);
+        project.classList.add("display");
+      }
+    });
+  });
+});
 
-
-    projects.forEach(project =>  {
-        console.log(project)
-        project.classList.remove('display')
-        if(project.classList.contains(chosen)) {
-            // project.classList.add('display')
-            console.log(project.classList)
-            project.classList.add('display')
-        }
-
-    })
-  
-})})
+// Under the hood hovers
